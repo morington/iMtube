f6d03295b07029a8f01852f26eae8526a083d7b0
morington <morington.mail@gmail.com>
2024-09-15 00:14:21 +0300

    GPT o1 stopped working because the limits were reached, we had to make improvements to the ideal of the first version manually using GPT-4o. She's stupid..

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
         logging.StreamHandler()
     ]
 )
